75d2b4b2eae0133e8586a37fc2c80474340733c8
András B Nagy <20251272+BNAndras@users.noreply.github.com>
2025-10-25 22:42:12 -0700

    Bump to unitt 3 (#46)
    * respec to rspec
    
    * Set up initial test files
    
    * Bump arturo nightly and unitt versions
    
    * Update .giitignore
    
    * Update more unitt version references
    
    * Update run.sh
    
    * Bump Arturo nightly
    
    * EOL
    
    * Quote results flepath

--- bin/run.sh
+++ bin/run.sh
@@ -36,19 +36,40 @@ tmp_dir=$(mktemp -d -t "exercism-verify-${slug}-XXXXX")
 trap 'rm -rf "$tmp_dir"' EXIT
 cp -r "${solution_dir}/." "${tmp_dir}"
 cd "${tmp_dir}"
-
 jq -r '.files.test[]' .meta/config.json | while read -r test_file; do
     sed -i -E 's/(test|it).skip/\1/g' "${test_file}"
 done
 
+# Capture the output of the test run in case of failure or error
 test_output=$(arturo tester.art 2>&1)
 
-# Write the results.json file based on the exit code of the command that was 
-# just executed that tested the implementation file
-if [ $? -eq 0 ]; then
-    jq -n '{version: 1, status: "pass"}' > ${results_file}
+# Tweak the generated test file structure to valid Arturo code 
+sed -i \
+    -e 's/specs: \[/specs: @[/g' \
+    -e 's/tests: \[/tests: @[/g' \
+    -e 's/assertions: \[/assertions: @[@/g' ".unitt/tests/test-${slug}.art"
+
+# Check whether an error or failure occurred so we can pipe the captured output appropriately.
+cat > check-test-run-success.art << 'EOF'
+if empty? arg -> panic.unstyled.code:2 ""
+resultFile: ~".unitt/tests/test-|arg\0|.art"
+if not? file? resultFile -> panic.code:2 ""
+do resultFile
+if empty? specs -> panic.unstyled.code:2 ""
+testStatuses: flatten map specs 'describe ->
+                fold.seed:@[] describe\tests [acc test] ->
+                    append acc test\assertions\0\1
+if not? all? testStatuses -> panic.unstyled ""
+EOF
+
+arturo check-test-run-success.art "${slug}"
+result=$?
+if [ $result -eq 1 ]; then
+    jq -n --arg output "${test_output}" '{version: 1, status: "fail", message: $output}' > "${results_file}"
+elif [ $result -eq 2 ]; then
+    jq -n --arg output "${test_output}" '{version: 1, status: "error", message: $output}' > "${results_file}"
 else
-    jq -n --arg output "${test_output}" '{version: 1, status: "fail", message: $output}' > ${results_file}
+    jq -n '{version: 1, status: "pass"}' > "${results_file}"
 fi
 
 echo "${slug}: done"
